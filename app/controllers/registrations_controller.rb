b9d803893f6ba54508a38fc40928d09585804c5d
Emily Twist <emily.twist@btinternet.com>
2013-08-05 12:41:12 +0100

    User, keyholder and guest account creation sorted

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
   
-  # POST /resource
+   # POST /resource
   def create
     
     if current_user!=nil
@@ -12,39 +12,48 @@ class RegistrationsController < Devise::RegistrationsController
       build_resource(sign_up_params)
       resource.user_id = @user.id
       
-      if @user.keyholder_id.nil? && resource.keyholder
-        if resource.save
-          if resource.active_for_authentication?
-            set_flash_message :notice, :signed_up if is_navigational_format?
-            sign_up(resource_name, resource)
-            @user.keyholder_id = resource.id
-            @user.save
-            sign_out(resource)
-            flash[:notice] = "You have successfully added a keyholder to your account"  
-            respond_with resource, :location => after_sign_up_path_for(resource)
+      if @user.read_attribute("#{resource_name}_id").nil?
+  
+          if resource.save
+            if resource.active_for_authentication?
+              set_flash_message :notice, :signed_up if is_navigational_format?
+              sign_up(resource_name, resource)
+              @user.update_attribute("#{resource_name}_id", resource.id)
+              @user.save
+              sign_out(resource)
+              flash[:notice] = "You have successfully created a #{resource_name} for your account."
+              respond_with resource, :location => after_sign_up_path_for(resource)
+            else
+              set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+              expire_session_data_after_sign_in!
+              respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+            end
           else
-            set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-            expire_session_data_after_sign_in!
-            respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+            clean_up_passwords resource
+            respond_with resource
           end
-        else
-          clean_up_passwords resource
-          respond_with resource
-        end
-       elsif @user.keyholder_id
-        flash[:notice] = 'You have already created a keyholder for your account.'
-        redirect_to current_user
-      end
+          
+       else
+       flash[:notice] = "You have already created a #{resource_name} for this account."
+       redirect_to current_user
+          
+       end
+    elsif current_keyholder!=nil
+      @key = current_keyholder
+      @user = current_keyholder.user
+      build_resource(sign_up_params)
+      resource.user_id = @user.id
       
-      elsif @user.guest_id.nil? && resource.guest
-        if resource.save
+      if @user.guest_id.nil?
+  
+          if resource.save
             if resource.active_for_authentication?
               set_flash_message :notice, :signed_up if is_navigational_format?
               sign_up(resource_name, resource)
               @user.guest_id = resource.id
               @user.save
               sign_out(resource)
-              flash[:notice] = "You have successfully added a guest to your account"  
+              flash[:notice] = "You have successfully created a guest for the account."
               respond_with resource, :location => after_sign_up_path_for(resource)
             else
               set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
@@ -55,41 +64,15 @@ class RegistrationsController < Devise::RegistrationsController
             clean_up_passwords resource
             respond_with resource
           end
-         elsif @user.guest_id
-          flash[:notice] = 'You have already created a guest for your account.'
-          redirect_to current_user
-        end  
-        
-    if current_keyholder!=nil
-        @key = current_keyholder
-        build_resource(sign_up_params)
-        resource.user_id = @key.user.id
-        
-        if @key.guest_id.nil?
-          if resource.save
-            if resource.active_for_authentication?
-              set_flash_message :notice, :signed_up if is_navigational_format?
-              sign_up(resource_name, resource)
-              @key.user.guest_id = resource.id
-              @key.user.save
-              sign_out(resource)
-              flash[:notice] = "You have successfully added a guest to the account"  
-              respond_with resource, :location => after_sign_up_path_for(resource)
-            else
-              set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-              expire_session_data_after_sign_in!
-              respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-            end
-          else
-            clean_up_passwords resource
-            respond_with resource
-          end
-         elsif @key.user.guest_id
-          flash[:notice] = 'There is already a guest for this account.'
-          redirect_to current_keyholder
-        end
-      end
-    
+          
+       else
+       flash[:notice] = "There is already a guest for this account."
+       redirect_to current_keyholder
+          
+       end
+     
+    end
+      
   end
   
   def update
